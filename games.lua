7cb604015f0540f728977429c540bea02b355cdb
suannelson123 <nelsonsuan12@gmail.com>
2025-10-21 17:25:59 -0700

    ww

--- games.lua
+++ games.lua
@@ -2,7 +2,7 @@ return {
     ["72920620366355"] = {
         name = "Operation One",
         pid = 1332,
-        load = loadstring(game:HttpGet("https://github.com/suannelson123/op-modules/blob/main/Operation%20One/main.lua")),
+        load = loadstring(game:HttpGet("https://raw.githubusercontent.com/suannelson123/op-modules/refs/heads/main/Operation%20One/main.lua?token=GHSAT0AAAAAADJKC437POCWZJ7UGJVR3KA62HYEROQ")),
         bypass_ver = "1.0",
     },
 };
