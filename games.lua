6fc08673af30a0ffa3c6481c8db276eb98abeba1
suannelson123 <nelsonsuan12@gmail.com>
2025-10-21 17:05:32 -0700

    up

--- games.lua
+++ games.lua
@@ -1,7 +1,7 @@
 return {
     ["72920620366355"] = {
         name = "Operation One",
-        pid = 1330,
+        pid = 1332,
         load = loadstring(game:HttpGet("https://raw.githubusercontent.com/Chaddestdog/KLUB/refs/heads/main/Operation%20One/main.lua")),
         bypass_ver = "1.0",
     },
