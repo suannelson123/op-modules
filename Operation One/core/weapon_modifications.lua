1712fe984322b2caa037057fe741102f5fa26a94
Nelson Suan <143183161+suannelson123@users.noreply.github.com>
2025-11-03 02:52:31 -0800

    Update weapon_modifications.lua

--- Operation One/core/weapon_modifications.lua
+++ Operation One/core/weapon_modifications.lua
@@ -3,7 +3,9 @@ local settings = {
     recoil_x = 1,
     recoil_y = 1,
     no_spread = false,
-    fast_reload = false
+    fast_reload = false,
+    fast_firerate = false,     
+    firerate_multiplier = 2.0  
 };
 
 rawset(weapon_modifications, "weapon_modifications_settings", settings);
@@ -20,13 +22,35 @@ weapon_modifications.init = function()
 
     local old_tweenInfo_new = clonefunction(TweenInfo.new);
     hook_function(TweenInfo.new, newcclosure(function(...)
-        if (debug.info(3, 'n') == "recoil_function") then
+        local args = {...};
+        local caller_name = debug.info(3, 'n');
+
+        if (caller_name == "recoil_function") then
             debug.setstack(3, 5, (debug.getstack(3, 5) * settings.recoil_x));
             debug.setstack(3, 6, (debug.getstack(3, 6) * settings.recoil_y));
+
         elseif (debug.info(4, 'n') == "reload_begin" and typeof(debug.getstack(4, 6)) == "number" and settings.fast_reload) then
             debug.setstack(4, 6, (debug.getstack(4, 6) / 1.1));
+
+        elseif (caller_name == "fire" or caller_name == "shoot" or caller_name == "send_shoot") and settings.fast_firerate then
+            if (typeof(args[1]) == "number" and args[1] > 0) then
+                args[1] = args[1] / settings.firerate_multiplier;
+                return old_tweenInfo_new(unpack(args));
+            end
         end;
+
         return old_tweenInfo_new(...);
+    end));
+
+    local old_task_delay = clonefunction(task.delay);
+    hook_function(task.delay, newcclosure(function(delay_time, callback)
+        if settings.fast_firerate then
+            local caller = debug.info(2, 'n');
+            if (caller == "fire" or caller == "shoot" or caller == "primary_fire") and typeof(delay_time) == "number" then
+                delay_time = delay_time / settings.firerate_multiplier;
+            end
+        end
+        return old_task_delay(delay_time, callback);
     end));
 
 end;
