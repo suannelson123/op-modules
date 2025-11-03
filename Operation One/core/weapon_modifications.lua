28fc1859860f74099108e9064572b4826ea4fea1
Nelson Suan <143183161+suannelson123@users.noreply.github.com>
2025-11-03 02:29:00 -0800

    Modify weapon settings and adjust firerate handling

--- Operation One/core/weapon_modifications.lua
+++ Operation One/core/weapon_modifications.lua
@@ -4,7 +4,7 @@ local settings = {
     recoil_y = 1,
     no_spread = false,
     fast_reload = false,
-    firerate_multiplier = 1 
+    firerate = 1
 }
 
 rawset(weapon_modifications, "weapon_modifications_settings", settings)
@@ -20,20 +20,19 @@ weapon_modifications.init = function()
 
     local old_tweenInfo_new = clonefunction(TweenInfo.new)
     hook_function(TweenInfo.new, newcclosure(function(...)
-        if (debug.info(3, 'n') == "recoil_function") then
+        local n3, n4 = debug.info(3, 'n'), debug.info(4, 'n')
+
+        if (n3 == "recoil_function") then
             debug.setstack(3, 5, (debug.getstack(3, 5) * settings.recoil_x))
             debug.setstack(3, 6, (debug.getstack(3, 6) * settings.recoil_y))
-        elseif (debug.info(4, 'n') == "reload_begin" and typeof(debug.getstack(4, 6)) == "number" and settings.fast_reload) then
-            debug.setstack(4, 6, (debug.getstack(4, 6) / 1.1))
-        elseif (debug.info(3, 'n') == "fire_bullet" or debug.info(3, 'n') == "send_shoot") and settings.firerate_multiplier > 1 then
 
-                    for i = 5, 15 do
-        local val = debug.getstack(3, i)
-        if typeof(val) == "number" and val > 0.05 and val < 0.5 then
-            debug.setstack(3, i, val / settings.firerate_multiplier)
+        elseif (n4 == "reload_begin" and typeof(debug.getstack(4, 6)) == "number" and settings.fast_reload) then
+            debug.setstack(4, 6, (debug.getstack(4, 6) / 1.1))
+
+        elseif (n3 == "key3" or n4 == "key3") and typeof(debug.getstack(3, 5)) == "number" then
+            local currentDelay = debug.getstack(3, 5)
+            debug.setstack(3, 5, currentDelay / settings.firerate)
         end
-    end
-end
 
         return old_tweenInfo_new(...)
     end))
