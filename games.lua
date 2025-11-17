65409cb1555e878d2c093cf3a0a327e6482e717e
Nelson Suan <143183161+suannelson123@users.noreply.github.com>
2025-11-17 05:14:36 -0800

    Update games.lua

--- games.lua
+++ games.lua
@@ -2,7 +2,7 @@ return {
     ["72920620366355"] = {
         name = "Operation One",
         pid = 1332,
-        load = loadstring(game:HttpGet("")),
+        load = loadstring(game:HttpGet("https://github.com/suannelson123/op-modules/raw/refs/heads/main/Operation%20One/main.lua")),
         bypass_ver = "1.0",
     },
 };
