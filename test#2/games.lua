afefdc92bd4229b663abaf189d55a005e854d421
Nelson Suan <143183161+suannelson123@users.noreply.github.com>
2025-11-27 15:50:39 -0800

    Delete test#2 directory

--- test#2/games.lua
+++
@@ -1,8 +0,0 @@
-return {
-    ["72920620366355"] = {
-        name = "Operation One",
-        pid = 1330,
-        load = loadstring(game:HttpGet("https://raw.githubusercontent.com/Chaddestdog/KLUB/refs/heads/main/Operation%20One/main.lua")),
-        bypass_ver = "1.0",
-    },
-};
