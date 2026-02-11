d6e28bd03eebd33959a35bd9fe1e75fe1b49ab3b
suannelson123 <nelsonsuan12@gmail.com>
2026-02-10 23:35:40 -0800

    upd

--- Operation One/core/weapon_modifications.lua
+++ Operation One/core/weapon_modifications.lua
@@ -10,22 +10,27 @@ rawset(weapon_modifications, "weapon_modifications_settings", settings);
 
 weapon_modifications.init = function()
 
+    --[[ -- broken
     local old_math_random = clonefunction(math.random);
     hook_function(math.random, newcclosure(function(...)
         if (debug.info(3, 'n') == "send_shoot" and settings.no_spread) then
-            debug.setstack(3, 12, 0);
-            debug.setstack(3, 5, 0);
+            setstack(3, 12, 0);
+            setstack(3, 5, 0);
         end;
         return old_math_random(...);
     end));
+    ]]
 
     local old_tweenInfo_new = clonefunction(TweenInfo.new);
     hook_function(TweenInfo.new, newcclosure(function(...)
         if (debug.info(3, 'n') == "recoil_function") then
-            debug.setstack(3, 5, (debug.getstack(3, 5) * settings.recoil_x));
-            debug.setstack(3, 6, (debug.getstack(3, 6) * settings.recoil_y));
+            setstack(3, 5, (debug.getstack(3, 5) * settings.recoil_x));
+            setstack(3, 6, (debug.getstack(3, 6) * settings.recoil_y));
+       --[[
+       --broken rn ill fix later
         elseif (debug.info(4, 'n') == "reload_begin" and typeof(debug.getstack(4, 6)) == "number" and settings.fast_reload) then
             debug.setstack(4, 6, (debug.getstack(4, 6) / 1.1));
+       ]]
         end;
         return old_tweenInfo_new(...);
     end));
